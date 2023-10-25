f7642dc4e19274c3ea96eb5076be5bec1c40a3da
RaminCodes-sol <ramin.yousefpour94@gmail.com>
2023-10-25 10:39:56 +0330

    edited

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -3,13 +3,12 @@ const hre = require("hardhat");
 
 
 async function main() {
-  const message = await hre.ethers.deployContract("Message")
+  const message = await hre.ethers.deployContract("Message", ["Hello NewYork"])
   await message.waitForDeployment()
 
+  console.log(`message deployed to: ${message.target}`)
 
-  console.log(
-    `message deployed to: ${message.target}`
-  );
+  console.log(`Message: ${await message.getMessage()}`)
 
 }
 
